282f033e22bf2534b4eda8051d1d7f210a201bb2
uscitp-jeantu <tujean27@gmail.com>
2017-04-27 23:05:05 -0700

    was tesint out other text to speech through google, but might just end up doing it with the operating system

--- Lab/SpeechTest.py
+++ Lab/SpeechTest.py
@@ -3,6 +3,23 @@
 # tts.SetVoiceByName('MSSam')
 # tts.Speak("Hello, fellow Python programmer")
 
-from os import system
+# import pyttsx
+# engine = pyttsx.init()
+#
+# voices = engine.getProperty('voices')
+# for voice in voices:
+#    engine.setProperty('voice', voice.id)
+#    engine.say('The quick brown fox jumped over the lazy dog.')
+# engine.runAndWait()
 
-system("say My name is Jean")
+from os import system
+string = "My name is Jean"
+system("say " + string)
+
+# from gtts import gTTS
+# blabla = ("Spoken text")
+# tts = gTTS(text=blabla, lang='en')
+# tts.save("test.mp3")
+
+# import sounddevice as sd
+# sd.play("test.mp3", 44100)
