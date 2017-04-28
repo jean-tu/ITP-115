b0f337f185965a803c0e07cf320f01a02c49eacd
uscitp-jeantu <tujean27@gmail.com>
2017-04-27 23:04:16 -0700

    Working on the extra credit assignment

--- Assignments/ITP115_a10_Tu_Jean/Game.py
+++ Assignments/ITP115_a10_Tu_Jean/Game.py
@@ -1,10 +1,25 @@
 from Vampire import Vampire
 from Human import Human
 
+# use a for loop using range to access each Human object in the list and display a number
 def printHumans(humanList):
+    pass
 
 
+#ask the user to enter an index number of a human
+# include error checking to see if the number is within the range
+# Human can't have < 0 quarts & vamp can't have more than 5
+def performFeeding(humanList, vamp):
+    pass
+
+    #use the isAlive & isStuffed to check
+    #call suckBlood & print out a message that indicates if vampire is stuffed or human is dead
+
+
+#create the human objects (can be hardcoded)
+#vampire object - ask the user for the name & animal & initially starving
 def main():
-
+    pass
+    #Will use printHumans and performFeeding
 
 main()
