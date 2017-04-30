7ff537292267879b7fcab5c45ab18c89d09a29d1
uscitp-jeantu <tujean27@gmail.com>
2017-04-30 14:23:39 -0700

    finished the Extra Credit assignment

--- Assignments/ITP115_a10_Tu_Jean/Game.py
+++ Assignments/ITP115_a10_Tu_Jean/Game.py
@@ -4,8 +4,9 @@ from Human import Human
 # use a for loop using range to access each Human object in the list and display a number
 def printHumans(humanList):
     index = 0
+    print()
     for human in humanList:
-        print(index + " " + human.__str__())
+        print(str(index) + ":  " + human.__str__())
         index += 1 #incrementing for the next user
 
 
@@ -13,17 +14,21 @@ def printHumans(humanList):
 # include error checking to see if the number is within the range
 # Human can't have < 0 quarts & vamp can't have more than 5
 def performFeeding(humanList, vamp):
+    print()
     numHumans = len(humanList)
-
+    printHumans(humanList) # so that they can see who they have to choose from
     validChoice = False
     while validChoice == False:
         numChosen = input("Select a human: ")
         if numChosen.isdigit():
-            if numChosen < numHumans and numChosen >= 0:
+            numChosen = int(numChosen) #converting it into a number
+            if numChosen < numHumans and numChosen >= 0: #seeing if it is a number within the range
                 humanChosen = humanList[numChosen] #getting the human object
                 if humanChosen.isAlive():
-                    if not vamp.isStuffed:
+                    if not vamp.isStuffed():
+                        print() # to make it look cleaner
                         vamp.suckBlood(humanChosen)
+                        print() # just to make it look cleaner
                     #else the vamp is stuffed and can't drink anymore
                 print(vamp.__str__())
                 print(humanChosen.__str__())
@@ -40,7 +45,7 @@ def main():
     human1 = Human("Mary", 4, "A") #create the human objects (can be hardcoded)
     human2 = Human("Oscar", 3, "B")
     human3 = Human("Skye", 2, "O")
-    humanList = [human1,human2, human3 ] #adding the hard coded humans into a list to be used later on
+    humanList = [human1,human2, human3] #adding the hard coded humans into a list to be used later on
 
     nameBad = True
     while nameBad:
@@ -49,7 +54,7 @@ def main():
             nameBad = False
     animalBad = True
     while animalBad:
-        vampAnimal = input("What is the animal of the Vampire?")
+        vampAnimal = input("What is the animal of the Vampire? ")
         if vampAnimal != " ":
             animalBad = False
     vamp = Vampire(vampName, 0, vampAnimal)#vampire object - ask the user for the name & animal & initially starving
@@ -59,23 +64,28 @@ def main():
     while cont:
         choiceValid = False
         while choiceValid == False:
+            # print()
             print("1) Print humans")
             print("2) Suck blood")
             choice = input("3) Exit ")
             if choice.isdigit():
+                choice = int(choice) #converting it into a number2
                 if choice == 1 or choice == 2 or choice == 3: #if it is a valid choice
-                    print("in CHOICE loop" ) ##debugg
-
-                    if choiceValid == 1:
-                        print("Choose 1") #debugg
+                    if choice == 1:
                         printHumans(humanList)
-                    elif choiceValid == 2:
-                        print("Choose 2") #debugg
+                    elif choice == 2:
                         performFeeding(humanList, vamp)
                     else: # the user wants to stop playing
                         print("Thanks for playing")
                         print(vampName + " transforms into " + vampAnimal)
                         cont = False
+                        choiceValid = True
+                else:
+                    print("Please choose a valid number")
+                    print()
+            else:
+                print("Please enter a number")
+                print()
 
 
     #Will use printHumans and performFeeding
